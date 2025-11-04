be25fce8a6ccb56679cf2bbe2503247ad8b8a9aa
Likhil N Maiya <134200383+Lickhill@users.noreply.github.com>
2025-11-04 14:18:47 +0530

    exp8

--- exp8/exp8.sql
+++ exp8/exp8.sql
@@ -12,18 +12,14 @@ CREATE TABLE students (
 
 DO $$
 BEGIN TRANSACTION 
-    -- Start a transaction
     BEGIN 
-        -- Insert multiple students
         INSERT INTO students(name, age, class) VALUES ('Anisha',16,8);
         INSERT INTO students(name, age, class) VALUES ('Neha',17,8);
         INSERT INTO students(name, age, class) VALUES ('Mayank',19,9);
 
-        -- If all succeed
         RAISE NOTICE ' Transaction Successfully Done';
 
     EXCEPTION WHEN OTHERS THEN
-            -- If any insert fails
             RAISE NOTICE 'Transaction Failed..! Rolling back changes.';
             RAISE;  -- this will rollback the entire transaction
     END;
@@ -38,21 +34,17 @@ SELECT * FROM students;
 --VIOLATED SCENARIO
 DO $$
 BEGIN TRANSACTION 
-    -- Start a transaction
     BEGIN 
-        -- Insert multiple students
         INSERT INTO students(name, age, class) VALUES ('Anisha',16,8);
 		INSERT INTO students(name, age, class) VALUES ('Mayank',19,9);
         INSERT INTO students(name, age, class) VALUES ('Anisha',17,8); --ERROR
         INSERT INTO students(name, age, class) VALUES ('Mayank',19,9);
 
-        -- If all succeed
         RAISE NOTICE ' Transaction Successfully Done';
 
     EXCEPTION WHEN OTHERS THEN
-            -- If any insert fails
             RAISE NOTICE 'Transaction Failed..! Rolling back changes.';
-            RAISE;  -- this will rollback the entire transaction
+            RAISE; 
     END;
 END;
 $$;
